d548295a52fb1903772e570ccaf55fa69e22716d
Mirko Usai <mirko.a.usai@avanade.com>
2018-11-08 08:57:01 +0100

    first working version

--- TrayApp/SkypeSignal/Constants/ColourStates.cs
+++ TrayApp/SkypeSignal/Constants/ColourStates.cs
@@ -1,15 +1,39 @@
 ﻿
 namespace SkypeSignal
 {
-        public static class ColourStates
+    public static class ColourStates
+    {
+        public const string Off = "0";
+        public const string Green = "1";
+        public const string Red = "2";
+        public const string Purple = "3";
+        public const string Yellow = "4";
+        public const string RedFadeInACall = "5";
+        public const string BlueIncomingCall = "6";
+        public const string PartyStrobe = "7";
+
+        public static string GetNameFromCode(string Code)
         {
-            public const string Off                 = "0";
-            public const string Green               = "1";
-            public const string Red                 = "2";
-            public const string Purple              = "3";
-            public const string Yellow              = "4";
-            public const string RedFadeInACall      = "5";
-            public const string BlueIncomingCall    = "6";
-            public const string PartyStrobe         = "7";
+            switch (Code)
+            {
+                case "0":
+                    return "Off";
+                case "1":
+                    return "Green";
+                case "2":
+                    return "Red";
+                case "3":
+                    return "Purple";
+                case "4":
+                    return "Yellow";
+                case "5":
+                    return "RedFadeInACall";
+                case "6":
+                    return "BlueIncomingCall";
+                default:
+                    break;
+            }
+            return null;
         }
+    }
 }
--- TrayApp/SkypeSignal/Senders/HubSender.cs
+++ TrayApp/SkypeSignal/Senders/HubSender.cs
@@ -1,17 +1,29 @@
 ﻿using SkypeSignal.Interfaces;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using System.Configuration;
 
-namespace SkypeSignal 
+namespace SkypeSignal
 {
     class HubSender : ISender
     {
         public void SendData(string Command)
         {
-            throw new NotImplementedException();
+            Task test = Run(ColourStates.GetNameFromCode(Command).ToLower());
+            test.Wait();
+        }
+
+        private async Task Run(string input)
+        {
+            var connectionString = ConfigurationManager.AppSettings["ConnectionString"];
+            var serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+
+            var cloudToDeviceMethod = new CloudToDeviceMethod(input);
+
+            var response = await serviceClient.InvokeDeviceMethodAsync("esp", cloudToDeviceMethod);
+
+            var json = response.GetPayloadAsJson();
         }
     }
 }
